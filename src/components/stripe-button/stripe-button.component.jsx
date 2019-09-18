ddf85b6801ba246421dd652868e14154a46a6395
toddmath <tmatheson11186@gmail.com>
2019-09-17 22:34:30 -0700

    erased new stripe code to go back with it unconnected to the stripe service, because Netlify cannot host express app 😦

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,43 +1,25 @@
-/* eslint-disable object-shorthand */
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
 
 /* cSpell: Disable */
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, ...props }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_Y8ZezDGRQ20LEe87XaHQJqXE00nniaQD75';
 
   const onToken = token => {
-    // console.log(token);
-    // alert('Payment Successful');
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token,
-      },
-    })
-      .then(response => {
-        console.log(`StripeCheckout Response => ${response}`);
-        alert('Succsessful Payment');
-      })
-      .catch(error => {
-        console.log(`Paymen Error: ${JSON.parse(error)}`);
-        alert(
-          'There was an issue with your payment! Make sure to use the provided credit card.'
-        );
-      });
+    console.log(props);
+    console.log(`[stripe token]: ${token}`);
+    alert('Payment Successfull!');
   };
 
   return (
     <StripeCheckout
       label='Pay Now'
       name='CRWN Clothing Ltd.'
+      allowRememberMe={false}
       billingAddress
       shippingAddress
-      image='../../assets/crown.svg'
+      image='https://svgshare.com/i/CUz.svg'
       description={`Your total is ${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
