d63bdef289fe55e082ec81d191bac81e6dce4bfe
toddmath <tmatheson11186@gmail.com>
2019-10-18 21:27:23 -0700

    Changed all lazy imports to regular inside App to try and fix this persistant bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useEffect, lazy, Suspense } from 'react';
+import React, { useEffect } from 'react';
 import posed, { PoseGroup } from 'react-pose';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import Header from './components/header/header.component';
-import Spinner from './components/spinner/spinner.component';
+// import Spinner from './components/spinner/spinner.component';
 import ErrorBoundary from './components/error-boundary/error-boundary.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyle } from './global.styles';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import CheckOutPage from './pages/checkout/checkout.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 // const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
-const ShopPage = lazy(() => import('./pages/shop/shop.component'));
-const CheckOutPage = lazy(() => import('./pages/checkout/checkout.component'));
-const SignInAndSignUpPage = lazy(() =>
-  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
-);
+// const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+// const CheckOutPage = lazy(() => import('./pages/checkout/checkout.component'));
+// const SignInAndSignUpPage = lazy(() =>
+//   import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+// );
 
 const RoutesContainer = posed.div({
   enter: { opacity: 1, delay: 300, beforeChildren: true },
@@ -35,34 +38,32 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <ErrorBoundary>
-        <Suspense fallback={<Spinner key='spinner' />}>
-          <PoseGroup>
-            <RoutesContainer key={location.key}>
-              <Switch location={location}>
-                <Route exact path='/' component={HomePage} key='home' />
-                <Route path='/shop' component={ShopPage} key='shop' />
-                <Route
-                  exact
-                  path='/checkout'
-                  component={CheckOutPage}
-                  key='checkout'
-                />
-                <Route
-                  exact
-                  path='/signin'
-                  key='signin'
-                  render={() =>
-                    currentUser ? (
-                      <Redirect key='homesignin' from='/signin' to='/' />
-                    ) : (
-                      <SignInAndSignUpPage key='signin' />
-                    )
-                  }
-                />
-              </Switch>
-            </RoutesContainer>
-          </PoseGroup>
-        </Suspense>
+        <PoseGroup>
+          <RoutesContainer key={location.key}>
+            <Switch location={location}>
+              <Route exact path='/' component={HomePage} key='home' />
+              <Route path='/shop' component={ShopPage} key='shop' />
+              <Route
+                exact
+                path='/checkout'
+                component={CheckOutPage}
+                key='checkout'
+              />
+              <Route
+                exact
+                path='/signin'
+                key='signin'
+                render={() =>
+                  currentUser ? (
+                    <Redirect key='homesignin' from='/signin' to='/' />
+                  ) : (
+                    <SignInAndSignUpPage key='signin' />
+                  )
+                }
+              />
+            </Switch>
+          </RoutesContainer>
+        </PoseGroup>
       </ErrorBoundary>
     </div>
   );
